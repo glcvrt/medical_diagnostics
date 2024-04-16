d75ebc9f0007ef3a3980c87dfc14a205efc33e43
nikita <i1472138@yandex.ru>
2024-04-16 10:30:18 +0700

    second commit

--- user/urls.py
+++ user/urls.py
@@ -2,19 +2,24 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from user.apps import UserConfig
-from user.views import RegisterView, ProfileView, VerificationTemplateView, generate_new_password, \
-    ErrorVerificationTemplateView, UserLogout
+from user.views import RegisterView, generate_new_password, \
+    UserLogout, UserLogin, users, UserConfirmEmailView, UserConfirmedView, \
+    UserConfirmationFailView, regenerate_password, UserConfirmationSentView, UserUpdateView, UserDetailView
 
 app_name = UserConfig.name
 
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('users', users),
+    path('login/', UserLogin.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    # path('logout/', UserLogout.as_view(next_page='diagnostics:home_page'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('verify_email/', VerificationTemplateView.as_view(), name='verify_email'),
+    path('confirm_email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
+    path('email_confirmed/', UserConfirmedView.as_view(), name='email_confirmed'),
+    path('email_confirmation_failed/', UserConfirmationFailView.as_view(), name='email_confirmation_failed'),
     path('profile/genpassword/', generate_new_password, name='generate_new_password'),
-    path('verify_email/error/', ErrorVerificationTemplateView.as_view(), name='verify_email_error'),
+    path('regenerations/', regenerate_password, name='regenerate_password'),
+    path('email_confirmation_sent/', UserConfirmationSentView.as_view(), name='email_confirmation_sent'),
+    path('profile/update/', UserUpdateView.as_view(), name='profile_update'),
+    path('profile/', UserDetailView.as_view(), name='profile'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,17 +1,34 @@
 import random
 
 from django.conf import settings
-from django.contrib.auth import logout
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import logout, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import LogoutView, LoginView, PasswordResetDoneView
 from django.core.mail import send_mail
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy, reverse
+from django.utils.crypto import get_random_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import View
 from django.views.generic import CreateView, UpdateView, TemplateView
 
 from user.forms import UserRegisterForm, UserProfileForm
 from user.models import User
 
 from django.utils.translation import gettext_lazy as _
+
+
+
+def users(request):
+    user = User.objects.all()
+    return render(request, 'user/user.html', {'user': user})
+
+
+class UserLogin(LoginView):
+    template_name = 'user/login.html'
+    success_url = reverse_lazy('diagnostics:home_page')
 
 
 class UserLogout(LogoutView):
@@ -46,58 +63,119 @@ class UserLogout(LogoutView):
 class RegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
-    template_name = 'user/register.html'
-    success_url = reverse_lazy('user:verify_email')
+    success_url = reverse_lazy('user:login')
 
     def form_valid(self, form):
-        if form.is_valid():
-            new_user = form.save()
-            send_mail(
-                subject='Подтверждение почты',
-                message=f'Код {new_user.ver_code}',
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[new_user.email]
-            )
-        return super().form_valid(form)
+        user = form.save()
+        user.is_active = False
+        user.save()
+
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        activation_url = reverse_lazy('user:confirm_email', kwargs={'uidb64': uid, 'token': token})
+
+        current_site = '127.0.0.1:8000'
+
+        send_mail(
+            subject='Регистрация на платформе',
+            message=f"Завершите регистрацию, перейдя по ссылке: http://{current_site}{activation_url}",
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return redirect('user:email_confirmation_sent')
 
 
-class ProfileView(UpdateView):
+class UserConfirmEmailView(View):
+
+    def get(self, request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64)
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            login(request, user)
+            return redirect('user:email_confirmed')
+        else:
+            return redirect('user:email_confirmation_failed')
+
+
+class UserConfirmedView(TemplateView):
+    """ Выводит информацию об успешной регистрации пользователя """
+    template_name = 'user/registration_confirmed.html'
+
+
+class UserConfirmationFailView(View):
+    """ Выводит информацию о невозможности зарегистрировать пользователя """
+    template_name = 'user/email_confirmation_failed.html'
+
+
+class UserConfirmationSentView(PasswordResetDoneView):
+    """ Выводит информацию об отправке на почту подтверждения регистрации """
+    template_name = "user/registration_sent_done.html"
+
+
+def generate_new_password(request):
+    """ Генерирует новый пароль пользователя """
+    new_password = get_random_string(length=9)
+
+    send_mail(
+        subject='Новый пароль',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+
+    request.user.set_password(new_password)
+    request.user.save()
+
+    return redirect(reverse('catalog:home'))
+
+
+def regenerate_password(request):
+    """ Генерирует новый пароль пользователя """
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        # Получаем пользователя по email
+        user = User.objects.get(email=email)
+
+        # Генерируем новый пароль
+        new_password = get_random_string(length=9)
+
+        # Изменяем пароль пользователя
+        user.set_password(new_password)
+        user.save()
+
+        # Отправляем письмо с новым паролем
+        send_mail(
+            subject='Восстановление пароля',
+            message=f'Ваш новый пароль: {new_password}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[email],
+            fail_silently=False,
+        )
+
+        return redirect(reverse('catalog:home'))
+    return render(request, 'user/regenerate_password.html')
+
+
+class UserUpdateView(UpdateView):
     model = User
-    form_class = UserProfileForm
     success_url = reverse_lazy('user:profile')
+    form_class = UserProfileForm
 
     def get_object(self, queryset=None):
         return self.request.user
 
 
-def generate_new_password(request):
-    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
-    send_mail(
-        subject='Вы сменили пароль',
-        message=f'Ваш новый пароль {new_password}',
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=[request.user.email]
-    )
-    request.user.set_password(new_password)
-    request.user.save()
-    return redirect(reverse('diagnostics:home_page'))
+class UserDetailView(LoginRequiredMixin, View):
 
-
-class VerificationTemplateView(TemplateView):
-    template_name = 'user/msg_email.html'
-
-    def post(self, request):
-        ver_code = request.POST.get('ver_code')
-        user_code = User.objects.filter(ver_code=ver_code).first()
-
-        if user_code is not None and user_code.ver_code == ver_code:
-            user_code.is_active = True
-            user_code.save()
-            return redirect('user:login')
-        else:
-            return redirect('user:verify_email_error')
-
-
-class ErrorVerificationTemplateView(TemplateView):
-    template_name = 'user/verify_email_error.html'
-    success_url = reverse_lazy('user:msg_email')
+    def get(self, request):
+        user = request.user
+        context = {
+            'object': user,
+        }
+        return render(request, 'user/profile.html', context)
